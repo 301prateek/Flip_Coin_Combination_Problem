846eb62bf1ab7c42067f823b10fa1c8017d3db88
301prateek <prateek.patil301@gmail.com>
2020-10-18 20:16:00 +0530

    Added: Dictionary and Percentage Calculation

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -5,12 +5,30 @@
 #Welcome message
 echo "Welcome to Flip Coin Combination"
 
-#Flipping coin to display heads or tails.
-coinFlip=$((RANDOM%2))
+#Flipping coin to display heads or tails storing results in dictionary.
+declare -A heads
+declare -A tails
 
-if [ $coinFlip -eq 1 ]
-then
-	echo "Heads"
-else
-	echo "Tails"
-fi
+headCount=0
+tailCount=0
+
+for(( i=0; i<20; i++ ))
+do
+	coinFlip=$((RANDOM%2))
+
+	if [ $coinFlip -eq 1 ]
+	then
+		echo "Heads"
+		headCount=$(($headCount+1))
+		heads[0]=$headCount
+	else
+		echo "Tails"
+		tailCount=$(($tailCount+1))
+		tails[0]=$tailCount
+	fi
+done
+echo "Heads: ${heads[@]}"
+echo "Tails: ${tails[@]}"
+
+echo "Percentage of Heads: $(((${heads[@]}*100)/20))"
+echo "Percentage of Heads: $(((${tails[@]}*100)/20))"
