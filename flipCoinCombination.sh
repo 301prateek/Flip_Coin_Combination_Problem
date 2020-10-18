f64d7ad4c4a1b585f7df3cf6c2bed650950b998d
301prateek <prateek.patil301@gmail.com>
2020-10-18 23:34:31 +0530

    Added: Triplet Combination and percentage

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -72,3 +72,73 @@ echo "HH percentage: $(((${HH[@]}*100)/20))"
 echo "HT percentage: $(((${HT[@]}*100)/20))"
 echo "TH percentage: $(((${TH[@]}*100)/20))"
 echo "TT percentage: $(((${TT[@]}*100)/20))"
+
+#Triplet combination
+
+declare -A HHH
+declare -A HHT
+declare -A HTH
+declare -A THH
+declare -A TTT
+declare -A TTH
+declare -A THT
+declare -A HTT
+
+hhh=0
+hht=0
+hth=0
+thh=0
+ttt=0
+tth=0
+tht=0
+htt=0
+
+for(( i=0; i<20; i++ ))
+do
+	coin1=$((RANDOM%2))
+	coin2=$((RANDOM%2))
+	coin3=$((RANDOM%2))
+	if [ $coin1 -eq 1 -a $coin2 -eq 1 -a $coin3 -eq 1 ]
+	then
+		hhh=$(($hhh + 1))
+		HHH[0]=$hhh
+	elif [ $coin1 -eq 1 -a $coin2 -eq 1 -a $coin3 -eq 0 ]
+	then
+		hht=$(($hht + 1))
+		HHT[0]=$hht
+	elif [ $coin1 -eq 1 -a $coin2 -eq 0 -a $coin3 -eq 1 ]
+	then
+		hth=$(($hth + 1))
+		HTH[0]=$hth
+	elif [ $coin1 -eq 0 -a $coin2 -eq 1 -a $coin3 -eq 1 ]
+	then
+		thh=$(($thh + 1))
+		THH[0]=$thh
+	elif [ $coin1 -eq 0 -a $coin2 -eq 0 -a $coin3 -eq 0 ]
+	then
+		ttt=$(($ttt + 1))
+		TTT[0]=$ttt
+	elif [ $coin1 -eq 0 -a $coin2 -eq 0 -a $coin3 -eq 1 ]
+	then
+		tth=$(($tth + 1))
+		TTH[0]=$tth
+	elif [ $coin1 -eq 0 -a $coin2 -eq 1 -a $coin3 -eq 0 ]
+	then
+		tht=$(($tht + 1))
+		THT[0]=$tht
+	elif [ $coin1 -eq 1 -a $coin2 -eq 0 -a $coin3 -eq 0 ]
+	then
+		htt=$(($htt + 1))
+		HTT[0]=$htt
+	fi
+done
+echo
+echo "HHH percentage: $(((${HHH[@]}*100)/20))"
+echo "HHT percentage: $(((${HHT[@]}*100)/20))"
+echo "HTH percentage: $(((${HTH[@]}*100)/20))"
+echo "THH percentage: $(((${THH[@]}*100)/20))"
+echo "TTT percentage: $(((${TTT[@]}*100)/20))"
+echo "TTH percentage: $(((${TTH[@]}*100)/20))"
+echo "THT percentage: $(((${THT[@]}*100)/20))"
+echo "HTT percentege: $(((${HTT[@]}*100)/20))"
+
